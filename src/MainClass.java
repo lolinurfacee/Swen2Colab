936bff104588fd98162f8afa1e94d9c9cc96dd2c
Nina <nbarkat@avans.nl>
2017-04-19 13:25:31 +0200

    My second commit

--- src/MainClass.java
+++ src/MainClass.java
@@ -2,8 +2,8 @@
 public class MainClass {
 
 	public static void main(String[] args) {
-		// TODO 	-generated method stub
-		System.out.println("HELLO?");
+		// TODO -generated method stub
+		System.out.println("Hello Universe! ");
 	}
 
 }
