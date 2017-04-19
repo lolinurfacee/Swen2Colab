ba4f1750007b33c9d27e6b0787d076aed0d80c5f
“Your <you@example.com>
2017-04-19 13:25:33 +0200

    mY SECOND COMMIT

--- src/MainClass.java
+++ src/MainClass.java
@@ -3,7 +3,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO 	-generated method stub
-		System.out.println("HELLO?");
+		System.out.println("HELLO GALAXY?");
 	}
 
 }
